f2fa755d616a63f8da786898cab44c95dc4d2de4
ibrahimtunc <tuncibrahimalperen@gmail.com>
2020-09-18 17:37:34 +0200

    18.09 1-1000 Hz reduced integrate and fire scan plot updates

--- plot_mem_tau_ref_period.py
+++ plot_mem_tau_ref_period.py
@@ -16,10 +16,10 @@ import pandas as pd
 
 savepath = r'D:\ALPEREN\Tübingen NB\Semester 3\Benda\git\punitmodel\data'
 datapathes = os.listdir(savepath)
-paths = [ path[:5] == 'decay' for path in datapathes]
+paths = [ path[:5] == 'decay' and path[29]!='1' for path in datapathes]
 pathes = list(np.array(datapathes)[paths])
 
-fig, ax = plt.subplots(1,len(pathes))
+fig, ax = plt.subplots(1,3)
 
 for i, dataname in enumerate(pathes):
     data = pd.read_csv(savepath+'\\'+dataname)
@@ -34,8 +34,32 @@ for i, dataname in enumerate(pathes):
 
     fig.colorbar(img, ax=ax[i], shrink=0.5)
     #plt.gca().invert_yaxis()
-    ax[i].set_ylabel('membrane tau [ms]')
-    ax[i].set_xlabel('refractory period [ms]')
-    ax[i].set_title('f = %1.f Hz' %(eval(dataname[-9:-6])), y = 1.05)    
+    ax[i].set_title('f = %1.f Hz' %(eval(dataname[29:34])), y = 1.05)    
     ax[i].tick_params(axis='both', labelsize=8)
 plt.subplots_adjust(left=0.05, right=1, wspace=0.07)
+ax[0].set_ylabel('membrane tau [ms]')
+ax[0].set_xlabel('refractory period [ms]')
+
+
+newpaths = [ path[:5] == 'decay' and path[29]=='1' for path in datapathes]
+newpathes = list(np.array(datapathes)[newpaths])
+fig, ax = plt.subplots(1,4)
+
+for i, dataname in enumerate(newpathes):
+    data = pd.read_csv(savepath+'\\'+dataname)
+    print(data[:10], np.max(np.max(data)), np.min(np.min(data)))
+    img = ax[i].imshow(data, extent = [0,data.shape[0],
+                                          data.shape[1],0], vmin = 0, vmax = 13)
+    ax[i].xaxis.tick_top()
+    ax[i].set_xticks(np.arange(0,data.shape[0]+1,4))
+    ax[i].set_yticks(np.arange(0,data.shape[0]+1,4))
+    ax[i].set_xticklabels(np.round(np.logspace(np.log10(1), np.log10(1000), 6),1))
+    ax[i].set_yticklabels(np.round(np.logspace(np.log10(1), np.log10(1000), 6),1))
+
+    fig.colorbar(img, ax=ax[i], shrink=0.5)
+    #plt.gca().invert_yaxis()
+    ax[i].set_title('f = %1.f Hz' %(eval(dataname[29:30+i])), y = 1.1)    
+    ax[i].tick_params(axis='both', labelsize=8)
+plt.subplots_adjust(left=0.05, right=1, wspace=0.1)
+ax[0].set_ylabel('membrane tau [ms]')
+ax[0].set_xlabel('refractory period [ms]')
